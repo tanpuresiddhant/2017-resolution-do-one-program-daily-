29e2bd6bece1b485b1e08219ff280a8e03dbf23c
tanpuresiddhant <tanpuresiddhant@gmail.com>
2017-06-02 17:38:20 -0700

    Update Codechef4.java
    Final code for adding two number given in the linked list

--- Codechef4.java
+++ Codechef4.java
@@ -14,7 +14,7 @@ class Node{
         }
     }
     
-class Codechef4
+class Codechef
 {
     
     //static Node head;
@@ -48,25 +48,111 @@ class Codechef4
         }
     }
     
-    
+    public static Node addTwoNumbersFromLL(Node head1,Node head2){
+        Node head3 = null;
+        int sum=0;
+        int carry=0;
+        Node current1 = head1;
+        Node current2 = head2;
+        if(head1==null){
+            if(head2==null){
+                return null; 
+            }else{
+                return head2;
+            }
+        }else{
+            if(head2==null){
+                return head1;
+            }
+             
+            while(current1!=null&&current2!=null){
+                sum = current1.data + current2.data+carry;
+                //System.out.println(sum);
+                if(sum>=10){
+                    int temp = sum % 10;
+                    carry = sum / 10;
+                    head3 = appendNode(head3, temp);
+                }else{
+                    carry=0;
+                    head3 = appendNode(head3, sum);
+                }
+                
+                current1=current1.next;
+                current2=current2.next;
+                
+            }
+            if(current1!=null){
+               // System.out.println("I am here");
+                while(current1!=null){
+                   sum = current1.data + carry;
+                   if(sum>=10){
+                        int temp = sum % 10;
+                        carry = sum / 10;
+                        head3 = appendNode(head3, temp);
+                    }else{
+                        carry = 0 ;
+                        head3 = appendNode(head3, sum);
+                    } 
+                    current1 = current1.next;
+                }
+            }
+            if(current2!=null){
+                //System.out.println("I am here2");
+                while(current2!=null){
+                   sum = current2.data + carry;
+                   if(sum>=10){
+                        int temp = sum % 10;
+                        carry = sum / 10;
+                        head3 = appendNode(head3, temp);
+                    }else{
+                        carry = 0 ;
+                        head3 = appendNode(head3, sum);
+                    }
+                    current2 = current2.next;
+                }
+            }
+            if(carry!=0){
+                //System.out.println("I have a carry " + carry);
+               if(carry>=10){
+                   while(carry>=10){
+                       int temp = carry % 10;
+                       carry = carry / 10;
+                       head3 = appendNode(head3, temp);
+                   }
+                     head3 = appendNode(head3, carry);
+                }else{
+                    head3 = appendNode(head3, carry);
+                }
+            }
+        }
+        return head3;
+    }
     
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Node head1=null ;
 		Node head2=null ;
-		head1 = appendNode(head1, 7);
-		appendNode(head1, 1);
-		appendNode(head1, 6);
+		head1 = appendNode(head1, 9);
+		appendNode(head1, 9);
+		appendNode(head1, 9);
+		appendNode(head1, 9);
+		//appendNode(head1, 6);
 		
-		head2 = appendNode(head2, 5);
-		appendNode(head2, 9);
-		appendNode(head2, 2);
-		
+		head2 = appendNode(head2, 1);
+		//appendNode(head2, 9);
+		//appendNode(head2, 2);
+		System.out.println("first number is");
 		printNode(head1);
-		printNode(head2);
 		System.out.println("***********************************");
-		//printNode(head1);
+		System.out.println("Second number is");
+		printNode(head2);
 		//System.out.println("***********************************");
+		
+		Node head3 = addTwoNumbersFromLL(head1,head2);
+		System.out.println("***********************************");
+		System.out.println("Sum is");
+		printNode(head3);
+		System.out.println("***********************************");
 		
 		
 	}
